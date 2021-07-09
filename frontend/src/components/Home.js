ed995a9837929c9b2b776f41fc7064e7005858dc
jordan <jthomasw88>
2021-07-09 14:11:32 +0100

    firebase set up
    auth user set up

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BG from "../Images/ATLg.jpg";
+
+function Home() {
+  return (
+    <div>
+      <img src={BG} alt="BG"></img>
+    </div>
+  );
+}
+export default Home;
